2fbb70b2b62e81aa6f3b155f7b96fc5dd49705e1
EclipX412 <cngao412@gmail.com>
2021-04-26 09:30:51 +0800

    main

--- Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/xsim_webtalk.tcl
+++ Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/GAO/Desktop/Computer-Orgnization-ARM-/Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Apr 25 22:24:15 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Apr 26 09:30:29 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "cecd8efd-969f-4fd8-9218-3e7f0e520d34" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "45f87b32ebf94e8e93d27428927444d4" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "31" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "32" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Genuine Intel(R) CPU 0000 @ 2.60GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6968_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2717189200 -regid "" -xml C:/Users/GAO/Desktop/Computer-Orgnization-ARM-/Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/GAO/Desktop/Computer-Orgnization-ARM-/Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/GAO/Desktop/Computer-Orgnization-ARM-/Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.08_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6976_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3580380050 -regid "" -xml C:/Users/GAO/Desktop/Computer-Orgnization-ARM-/Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/GAO/Desktop/Computer-Orgnization-ARM-/Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/GAO/Desktop/Computer-Orgnization-ARM-/Project/3.regFile/regFile.sim/sim_1/behav/xsim/xsim.dir/tb_regFile_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
