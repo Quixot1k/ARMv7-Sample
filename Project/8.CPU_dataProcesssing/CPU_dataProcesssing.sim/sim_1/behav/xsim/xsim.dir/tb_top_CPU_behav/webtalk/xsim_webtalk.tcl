ec5aefe4596fc649205f68a3595ffcc579862acc
EclipX412 <cngao412@gmail.com>
2021-05-30 23:05:19 +0800

    main

--- Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/xsim_webtalk.tcl
+++ Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/gao/Desktop/Computer-Orgnization-ARM-/Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon May 17 19:06:35 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon May 24 03:19:59 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "38066e94-8641-4a8e-8197-4fc596b4f619" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "8c8ccd2a9a4b469db0d7b4a8956ccea5" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "30" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "36" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Genuine Intel(R) CPU 0000 @ 2.60GHz" -context "user_environment"
@@ -25,8 +25,8 @@ webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "7408_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 842852203 -regid "" -xml C:/Users/gao/Desktop/Computer-Orgnization-ARM-/Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/gao/Desktop/Computer-Orgnization-ARM-/Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/gao/Desktop/Computer-Orgnization-ARM-/Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key iteration -value "3" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6980_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 639328972 -regid "" -xml C:/Users/gao/Desktop/Computer-Orgnization-ARM-/Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/gao/Desktop/Computer-Orgnization-ARM-/Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/gao/Desktop/Computer-Orgnization-ARM-/Project/8.CPU_dataProcesssing/CPU_dataProcesssing.sim/sim_1/behav/xsim/xsim.dir/tb_top_CPU_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
